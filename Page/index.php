0dd3e5aba44c662b743a23ececa4906b3aead303
JacobEduardo <jparraguezv@SNO_ARL01>
2021-08-16 19:27:55 -0400

    terminadno la pagina

--- Page/index.php
+++ Page/index.php
@@ -7,8 +7,8 @@
         ?>
     </head>
 
-    <body class="bg-secondary">
-        <div class="container bg-light p-0">
+    <body class="bg-secondary " >
+        <div class="container bg-light p-0"style="box-shadow: 0px 5px 10px">
         <?php
             include("page.php");
             include("body/body.php");
--- Page/page.php
+++ Page/page.php
@@ -1,54 +1,31 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
+    <a class="navbar-brand" style="font-family: Oswald" href="#">Pastosintetico Uv</a>
+
   </div>
 </nav>
 
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="image/01.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+      <div class="text-center p-4 text-light" style=" position: absolute;  top: 40%;  left: 0;  width: 100%; text-shadow: 0.1em 0.1em 0.2em black">
+        <h5>Pastosintetico Uv</h5>
+        <p>Soluciones ornamentales en pasto sintetico.</p>
       </div>
     </div>
     <div class="carousel-item">
       <img src="image/02.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
+      <div class="text-center p-4 text-light" style=" position: absolute;  top: 40%;  left: 0;  width: 100%; text-shadow: 0.1em 0.1em 0.2em black ">
+        <h5>Aplicaciones</h3>
+        <p>Mejoramiento de canchas de fútbol, futbolito y baby fútbol.</p>
       </div>
     </div>
     <div class="carousel-item">
       <img src="image/03.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
+      <div class="text-center p-4 text-light" style=" position: absolute;  top: 40%;  left: 0;  width: 100%; text-shadow: 0.1em 0.1em 0.2em black">
+        <h5>Aplicaciones</h3>
+        <p>Parques, oficinas, tiendas comerciales parques y departamentos.</p>
       </div>
     </div>
   </div>
@@ -62,65 +39,76 @@
   </button>
 </div>
 
-<div class="container mt-4 mb-4">
+<div class="p-3 container text-center bg-dark">
   <div class="row">
     <div class="col-sm">
-      <div class="card">
+      <div class="card p-3 m-2 bg-dark text-light">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-bar-chart-fill" viewBox="0 0 16 16">
+  <path d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3zm5-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V2z"/>
+</svg>
+            <h5 class="card-title">Durabilidad</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Materiales altamente resistente de alta calidad.</h6>
           </div>
         </div>
     </div>
     <div class="col-sm">
-    <div class="card" >
+    <div class="card p-3 m-2  bg-dark text-light">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-calendar-check" viewBox="0 0 16 16">
+  <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
+</svg>
+            <h5 class="card-title">Rapides</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Presupuesto rapido y precios de alto competitividad</h6>
           </div>
         </div>
     </div>
     <div class="col-sm">
-    <div class="card" >
+    <div class="card p-3 m-2  bg-dark text-light">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-stack" viewBox="0 0 16 16">
+  <path d="m14.12 10.163 1.715.858c.22.11.22.424 0 .534L8.267 15.34a.598.598 0 0 1-.534 0L.165 11.555a.299.299 0 0 1 0-.534l1.716-.858 5.317 2.659c.505.252 1.1.252 1.604 0l5.317-2.66zM7.733.063a.598.598 0 0 1 .534 0l7.568 3.784a.3.3 0 0 1 0 .535L8.267 8.165a.598.598 0 0 1-.534 0L.165 4.382a.299.299 0 0 1 0-.535L7.733.063z"/>
+  <path d="m14.12 6.576 1.715.858c.22.11.22.424 0 .534l-7.568 3.784a.598.598 0 0 1-.534 0L.165 7.968a.299.299 0 0 1 0-.534l1.716-.858 5.317 2.659c.505.252 1.1.252 1.604 0l5.317-2.659z"/>
+</svg>
+            <h5 class="card-title">Instalacion</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Instalaciones rapidas con los mas altos enstandares de calidad</h6>
           </div>
         </div>
     </div>
   </div>
 </div>
 
-<form class="p-4 bg-white ">
-  <div class="form-group">
+<div class="bg-white">
+<div class="mx-auto" style="max-width: 500px;">
+      <form class="p-4 ">
+      <div class="form-group">
 
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nombre">
+        <label class="mb-3"><b>¿Tienes Dudas? Contacte con nosotros. </b></label></br>
 
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Correo Electronico">
+        <label for="exampleFormControlInput1">Nombre</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nombre" style="max-width: 350px;">
 
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Correo Electronico">
+        <label for="exampleFormControlInput1">Correo electronico</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Correo Electronico" style="max-width: 350px;">
 
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Mensaje</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
+        <label for="exampleFormControlInput1">Telefono</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Telefono" style="max-width: 350px;">
+
+      </div>
+      <div class="form-group">
+        <label for="exampleFormControlTextarea1">Mensaje</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+      <div class="mb-5">
+        <button type="button" class=" mt-3 btn btn-success" style="float: right;">Enviar</button>
+      </div>
 </form>
+</div>
+</div>
 
 <div class="bg-dark p-4 text-center">
-<label class="text-light" >Nombre empresa</label></br>
-<label class="text-light" >+5694563784</label>
+<label class="text-light" >Pastosintetico Uv</label></br>
+<label class="text-light" >+5694563784</label></br>
 <label class="text-light" >correoelectronico@correo.com</label></br>
 </div>
